d6ad4c32a3719f578e186a0113eef446db6a3b97
cjscode <cmoo8675@lps53.org>
2023-05-03 19:17:17 -0500

    v0.02

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("main.js has loaded")
+let game = {}
+game.money = 0
+game.jobCategories = {
+    standWorker:[],
+}
+function update () {
+    let jobBar = document.getElementById("div_job_progress")
+    requestAnimationFrame(update)
+}
+function getPercent(current, max) {
+    return (current/max)*100
+}
+window.onload = update()
