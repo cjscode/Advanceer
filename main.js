9861d059b4cfd60c04f2b0cfbd0f11bc963a178b
cjscode <cmoo8675@lps53.org>
2023-05-04 07:07:38 -0500

    commit v0.03

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 console.log("main.js has loaded")
+//game
 let game = {}
+//money
 game.money = 0
+//categores
 game.jobCategories = {
-    standWorkers:[],
+    standWorkers:{},
 }
+//update
 function update () {
     let jobBar = document.getElementById("div_job_progress")
     requestAnimationFrame(update)
 }
+//get percent
 function getPercent(current, max) {
     return (current/max)*100
 }
-window.onload = update()
+//start updates
+update()
